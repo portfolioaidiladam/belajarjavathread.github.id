2a0dfb788138b81c37905cb41a37d048a8d4b599
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-08-28 19:46:26 +0700

    Belajar CountDownlatch sampai dengan Concurrent Collection

---
+++ src/test/java/programmerzamannow/thread/CountDownLatchTest.java
@@ -0,0 +1,43 @@
+package programmerzamannow.thread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+// kalau mau bikin thread yang nunggu beberapa proses selesai
+public class CountDownLatchTest {
+
+  @Test
+  void test() throws InterruptedException {
+
+    final var countDownLatch = new CountDownLatch(5);
+    final var executor = Executors.newFixedThreadPool(10);
+
+    for (int i = 0; i < 5; i++) {
+      executor.execute(() -> {
+        try {
+          System.out.println("Start Task");
+          Thread.sleep(2000);
+          System.out.println("Finish Task");
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } finally {
+          countDownLatch.countDown();
+        }
+      });
+    }
+
+    executor.execute(() -> {
+      try {
+        countDownLatch.await();
+        System.out.println("Finish All Tasks");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+
+    executor.awaitTermination(1, TimeUnit.DAYS);
+
+  }
+}
